9f8bc8e66e9b1de5229f11b00aa805b4f1bf9de0
ntuan19 <nanhtuan.fly@gmail.com>
2022-12-09 09:38:08 -0300

    Add DFS for checking duplicates or linked information

--- backend/authentication_helper.py
+++ backend/authentication_helper.py
@@ -2,6 +2,7 @@ from app import clientCollection
 #check account duplicates.
 from dao.crud import Crud
 from flask import flash, redirect, url_for
+from collections import defaultdict
 
 list_numbers = ["1","2","3","4","5","6","7","8","9","0"]
 special_characters = ["!","@","#","$","%","^","&","*","~","_","-","+","="]
@@ -40,6 +41,45 @@ def check_sign_up(password,password_retype):
         raise Exception("Special char missing")
     if special_char and numeric_val:
         return True 
+
+
+class Solution:
+    def accountsMerge(self, accounts):
+        #create a adjacent lists that have connections 
+        dic = defaultdict(set)
+        email_to_name={}
+        visited = set()
+        ans = []
+        for account in accounts:
+            name = account[0]
+            for email in account[1:]:
+                dic[email].add(account[1])
+                dic[account[1]].add(email)
+                email_to_name[email] = name
+
+
+        def dfs(intial_email,dic,visited):
+            if intial_email in visited:
+                return 
+            stack = [intial_email]
+            visited.add(intial_email)
+            lst = []
+            lst.append(intial_email)
+            while stack:
+                sub_email = stack.pop()
+                if dic[sub_email]:
+                    for other_email in dic[sub_email]:
+                        if other_email not in visited:
+                            lst.append(other_email)
+                            stack.append(other_email)
+                            visited.add(other_email)
+            result = sorted(lst)
+            return result
+        for email in dic:
+            result = dfs(email,dic,visited)
+            if result:
+                ans.append([email_to_name[email]]+result)
+        return ans
 
 
 
